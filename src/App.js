5d65f6749d46178ee6d2e4bd74062c868539a88e
Rukshan123 <rukzmadu@gmail.com>
2021-10-18 12:03:57 +0530

    get movies from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './App.css'
+import Row from './Row'
+import requests from './request'
 
 function App() {
   return (
     <div className='App'>
-      <h1>hi</h1>
+      <Row
+        fetchUrl={requests.fetchNetflixOriginals}
+        title='NETFLIX ORIGINALS'
+      ></Row>
+      <Row fetchUrl={requests.fetchTrending} title='Trending Now'></Row>
     </div>
   )
 }
