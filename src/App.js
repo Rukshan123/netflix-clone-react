9f7a13cd749df60d51d151df2a0bfd74aeefb757
Rukshan123 <rukzmadu@gmail.com>
2021-10-18 12:42:49 +0530

    add all categories in netflix

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ function App() {
   return (
     <div className='App'>
       <Row
+        title='Netflix Originals'
+        isLargeRow
         fetchUrl={requests.fetchNetflixOriginals}
-        title='NETFLIX ORIGINALS'
-      ></Row>
-      <Row fetchUrl={requests.fetchTrending} title='Trending Now'></Row>
+      />
+      <Row title='Trending' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
