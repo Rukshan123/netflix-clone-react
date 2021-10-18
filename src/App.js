99bc038912fb4c2a480473e9a203c7b36924e10a
Rukshan123 <rukzmadu@gmail.com>
2021-10-18 13:03:31 +0530

    add styles in to our Row component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import requests from './request'
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Row
         title='Netflix Originals'
-        isLargeRow
+        isLargeRow={true}
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title='Trending' fetchUrl={requests.fetchTrending} />
